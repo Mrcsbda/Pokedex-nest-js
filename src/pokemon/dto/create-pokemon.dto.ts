9cf43da706fb89e9d3b566bc18a2df00d3f2d5c3
Mariana Castañeda Bedoya <macbedoya@gmail.com>
2025-01-21 10:26:17 -0500

    post pokemon and find pokemon by name, id or no

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -2,22 +2,12 @@ import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
 
 export class CreatePokemonDto {
 
-    @IsInt({
-        message: 'no must to be an integer',
-    })
-    @IsPositive(
-        {
-            message: 'no must to be an positive integer',
-        }
-    )
-    @Min(1, {
-        message: 'no must to be greater than or equal to 1',
-    })
+    @IsInt()
+    @IsPositive()
+    @Min(1)
     readonly no: number;
 
     @IsString()
-    @MinLength(1, {
-        message: 'name is too short',
-    })
-    readonly name: string;
+    @MinLength(1)
+    name: string;
 }
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,20 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { isValidObjectId, Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    console.log(createPokemonDto)
-    return 'This action adds a new pokemon';
+
+  constructor(
+
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.trim().toLowerCase();
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+      return pokemon;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`Pokemon already exists in db ${JSON.stringify(error.keyValue)}`);
+      }
+      console.log(error)
+      throw new InternalServerErrorException(`Can't create pokemon - check logs for more info`);
+    }
+
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(searchTerm: string) {
+    let pokemon: Pokemon;
+
+    if (!isNaN(+searchTerm)) {
+      pokemon = await this.pokemonModel.findOne({ no: searchTerm})
+    }
+
+    if(!pokemon && isValidObjectId(searchTerm)) {
+      pokemon = await this.pokemonModel.findById(searchTerm)
+    }
+
+    if(!pokemon) {
+      pokemon = await this.pokemonModel.findOne({ name: searchTerm.toLowerCase() })
+    }
+
+    if(!pokemon) throw new NotFoundException(`Pokemon with id, name or no "${searchTerm}" not found`)
+
+    return pokemon
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
